2ed787b0bc502fe8748b604aded078ac8a34d541
yuan <1212@121.com>
2022-12-25 14:41:10 +0800

    update: add test

--- src/main/java/cn/jxau/Main.java
+++ src/main/java/cn/jxau/Main.java
@@ -1,7 +1,19 @@
 package cn.jxau;
 
+import java.util.Optional;
+
+/**
+ * jdk8 optional test example
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        test(null);
     }
+
+    static void test(String s){
+        Optional.ofNullable(s).ifPresent(System.out::println);
+    }
+
 }
